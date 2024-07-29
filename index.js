5d1b3b4b6fb69a9515fc9de811443c8e8211925d
sonyerg <ericksonrguinto@gmail.com>
2024-07-30 10:35:15 +1200

    added css styles

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ marked.setOptions({
 
 const renderer = new marked.Renderer();
 const defaultMd = `
-# Hello
+# Header
 ## Subheader
 
 [@sonyerg](https://github.com/sonyerg)
@@ -24,18 +24,28 @@ function App() {
   const [text, setText] = React.useState(defaultMd);
 
   return (
-    <div className="text-center px-4">
-      <h1 className="p-4">Markdown Previewer</h1>
-      <textarea
-        name="text"
-        id="editor"
-        rows="10"
-        cols="50"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        className="textarea"
-      />
-      <h3 className="mt-3">Output</h3>
+    <div className="px-4">
+      <h1 className="p-4 text-center">Markdown Previewer</h1>
+      <div className="text-center">
+        <textarea
+          name="text"
+          id="editor"
+          rows="10"
+          cols="50"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className="textarea"
+        ></textarea>
+      </div>
+      <h3 className="mt-3 text-center">Output</h3>
+      <Previewer text={text} />
+    </div>
+  );
+}
+
+function Previewer({ text }) {
+  return (
+    <div className="preview-container">
       <div
         dangerouslySetInnerHTML={{
           __html: marked(text, { renderer: renderer }),
